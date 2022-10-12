4ba09c3a617e8113e9d648c282ecbcd1a0874e28
mengtingan <115604562+mengtingan@users.noreply.github.com>
2022-10-12 11:33:36 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  rectMode(CENTER)
+  noFill()
+  stroke(255)
+  strokeWeight(2)
+  for(var j = -1;j<int(height/(50));j++){
+    for(var i = -1;i<int(width/(50));i++)
+    {
+      stroke(frameCount%256,mouseX,mouseY)
+      ellipse(25+(i*50),25+(j*50),mouseX)
+      stroke(mouseY,mouseX,frameCount%256)
+      rect(25+(i*50),25+(j*50),mouseX/5)
+      stroke(mouseX,frameCount%256,mouseY)
+      ellipse(50+(i*50),50+(j*50),mouseX/10)
+      stroke(mouseX,mouseY,frameCount%256)
+      ellipse(0+(i*50),50+(j*50),mouseX/10)
+    }
+  }
+}
